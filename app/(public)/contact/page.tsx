111f4c23cdb45e3e725b0dc2163f1bc7b604dfe2
Jhonrs <jhonrs812@gmail.com>
2025-02-13 10:02:21 -0500

    First Commit

---
+++ app/(public)/contact/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'Contact SEO Title',
+ description: 'Contact SEO Description',
+};  
+
+export default function ContactPage(){
+    return(
+        <>
+        <span className="text-7xl">Contact Page</span>
+        </>
+    )
+}
